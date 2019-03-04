1bb1a7586e9e6f87f72c4472a49b414c20f1f8ef
Adam Tizzone <ad-t@users.noreply.github.com>
2019-03-05 08:52:39 +1100

    Added starting code for week 3 tutorial

---
+++ wk3/BSTree.c
@@ -0,0 +1,32 @@
+#include "BSTree.h"
+
+#include <stdlib.h>
+#include <assert.h>
+
+int main (int argc, char* argv[]) {
+  return EXIT_SUCCESS;
+} 
+
+Tree* newTree() {
+  // TODO - Implement!
+  return (Tree*) NULL;
+}
+
+void insertItem(Item item) {
+  // TODO - Implement!
+}
+
+int countInternal(Tree t) {
+  // TODO - Implement!
+  return -1;
+}
+
+int nodeDepth(Tree t, Item key) {
+  // TODO - Implement!
+  return -1;
+}
+
+int maxBranchLen(Tree t) {
+  // TODO - Implement!
+  return -1;
+}
---
+++ wk3/BSTree.h
@@ -0,0 +1,24 @@
+#define TRUE 1
+#define FALSE 0
+
+/*
+typedef struct BSTNode *Link;
+typedef struct BSTNode {
+  int  value;
+  Link left, right;
+} BSTNode;
+*/
+
+typedef struct Tree* SubTree;
+typedef struct Tree {
+  int value;
+  SubTree left, right;
+} Tree;
+
+typedef int Item;
+
+Tree* newTree();
+void insertItem(Item item);
+int countInternal(Tree t);
+int nodeDepth(Tree t, Item key);
+int maxBranchLen(Tree t);
